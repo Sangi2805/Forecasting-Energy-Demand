c627352996dcfa96577973a4398ec74f58e86d0a
Xuan Toan Doan <txdoan@mun.ca>
2026-07-18 08:25:56 -0230

    TFT with Quantile loss

--- src/test.py
+++ src/test.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 configure_mlflow_tracking()
 
-with mlflow.start_run(run_name="connection_test_Toan"):
+with mlflow.start_run(run_name="I_cannot connect to DagsHub_I_need_to_test"):
     mlflow.log_param("test", True)
     mlflow.log_metric("dummy_metric", 1.0)
 
